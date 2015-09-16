895db9777b32d7510bc17c24576a126559f6bd35
RodyRafa <me@rodyrafa.com.br>
2015-09-16 00:20:06 -0300

    [WIP] Improvements

--- src/game.js
+++ src/game.js
@@ -1,27 +1,42 @@
 (function(root){
   'use strict';
   var HexicGame = function (options) {
-    this.game = document.getElementById('hexic-game');
-    this.createGrid(17, 5);
-    
+    this.prepare(options);
+    this.initGame(options);
+  };
+
+  HexicGame.prototype.initGame = function (options) {
+    this.createGrid(options.grid.col, options.grid.row);
+  };
+
+  HexicGame.prototype.prepare = function (options) {
+    this.game = document.getElementById(options.game);
+    this.arrPons = [];
+    this.rowPair = [];
   };
   
-  HexicGame.prototype.createGrid = function (col, row) {
+  HexicGame.prototype.createGrid = function (row, col) {
     var posX = 0;
     var posY = 50;
     var len = col * row;
-    for(var i=1; i< len+1; i++) {
+    var booAdd = true;
+    for(var i=0; i< len; i++) {
       var pon = this.createHexagon(i);
       pon.style.marginLeft = posX+'px';
       pon.style.marginTop = posY+'px';
       posX += 92;
-      if(i % row == 0){
+      if((i+1) % row == 0 && i != 0){
         posX = 92 / 2;
         posY += 26; 
-        if(i % 2 == 0) {
+        booAdd = !booAdd;
+        if((i+1) % 2 == 0) {
           posX = 0;
         }
       }
+      if(booAdd) {
+        this.rowPair.push(i);
+      }
+      this.arrPons.push(pon);
     }
   };
 
@@ -29,8 +44,83 @@
     var pon = document.createElement('DIV');
     pon.setAttribute('class', 'hex');
     pon.setAttribute('id', id);
+    
+    this.addEvent(pon, 'click', this.onClick.bind(this));
     this.game.appendChild(pon); 
     return pon;
+  };
+
+  HexicGame.prototype.addEvent = function (el, evnt, funct) {
+    if(el.attachEvent) {
+      return el.attachEvent('on'+evnt, funct);
+    } else {
+      return el.addEventListener(evnt, funct, false);
+    }
+  };
+
+  HexicGame.prototype.onClick = function (event) {
+    console.log(event.target.id);
+    this.onHit(event.target.id);
+  };
+
+  HexicGame.prototype.checkPairRow = function (id) {
+    for(var i = 0; i < this.rowPair.length; i++) {
+      if(id == this.rowPair[i]) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  HexicGame.prototype.onHit = function (id) {
+    id = Number(id);
+    this.setColor(this.arrPons[id]);
+    this.setColor(this.arrPons[this.getIdTop(id)]);
+    this.setColor(this.arrPons[this.getIdBottom(id)]);
+    this.setColor(this.arrPons[this.getIdTopLeft(id)]);
+    this.setColor(this.arrPons[this.getIdTopRight(id)]);
+    this.setColor(this.arrPons[this.getIdBotLeft(id)]);
+    this.setColor(this.arrPons[this.getIdBotRight(id)]);
+  };
+
+  HexicGame.prototype.setColor = function (el) {
+    el.setAttribute('class', 'hex blue');
+  }
+
+  HexicGame.prototype.getIdTop = function (id) {
+    return id - 10 > -1 ? id - 10 : null;
+  }
+
+  HexicGame.prototype.getIdBottom = function (id) {
+    return id + 10 <= this.arrPons.length ? id + 10 : null;
+  }
+
+  HexicGame.prototype.getIdTopLeft = function (id) {
+    var distId = 6;
+    if(!this.checkPairRow(id)) {
+      distId = 5;
+    }
+    return id - distId > 0 ? id - distId : null;
+  };
+
+  HexicGame.prototype.getIdTopRight = function (id) {
+    var distId = 5;
+    if(!this.checkPairRow(id)) {
+      distId = 4;
+    }
+    return id - distId > 0 ? id - distId : null;
+  };
+
+  HexicGame.prototype.getIdBotLeft = function (id) {
+    var distId = 4;
+    if(!this.checkPairRow(id)) {
+      distId = 6;
+    }
+    return id + distId <= this.arrPons.length ? id + distId : null;
+  };
+
+  HexicGame.prototype.getIdBotRight = function (id) {
+    return id + 5 <= this.arrPons.length ? id + 5 : null;
   };
 
   root.HexicGame = HexicGame;
